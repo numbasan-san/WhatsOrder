5d9c35bb06d0be21598adc7d58e3d56e386ab599
numbasan-san <80220352+numbasan-san@users.noreply.github.com>
2026-06-11 02:53:35 -0400

    delete backend directory

--- backend/app/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-file: backend/app/utils/__init__.py [2026/06/04]
-last mod: 2026/06/04
-recent mods:
-    - 2026/06/04 [Eddy M - BackEnd]: file created.
-"""
-
-from .auth import get_current_user, security
